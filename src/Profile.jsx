cc1c9b535f3cb1886a54c2dfa18c607748e004be
citylightss <citylightss@gmail.com>
2022-08-29 14:15:58 +0300

    add Profile.jsx

---
+++ src/Profile.jsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+	return (
+		<div className="row">
+			<div className="col-sm-4"><img src="" alt="" /></div>
+      <div className="col-sm-8"></div>
+		</div>
+	);
+};
+
+export default Profile;
